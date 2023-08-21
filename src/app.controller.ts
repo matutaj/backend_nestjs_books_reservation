87a9d781baa11f9b3f6435d29c54328b2a818e68
matutaj <matutajorge2002@gmail.com>
2023-08-21 21:42:39 +0100

    feat: add repositories

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
-import { randomUUID } from "node:crypto"
 import { CreateTeamMemberBody } from './dtos/create-team-member-rocket';
+import { RocketTeamMember } from './repositories/Rocket-member-repository';
 @Controller("app")
 export class AppController {
   constructor(
-    private prisma: PrismaService
+    private rocketMemberRepository: RocketTeamMember
   ) { }
 
-  @Get("member")
+  @Post("member")
   async CreateMember(@Body() body: CreateTeamMemberBody) {
     const { name, function: memberFunction } = body
-    const member = await this.prisma.rocketTeamMember.create({
-      data: {
-        id: randomUUID(),
-        name,
-        function: memberFunction
-      }
-    })
-    return member;
+    await this.rocketMemberRepository.create(name, memberFunction)
+
   }
+
+
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaService } from './database/prisma.service';
+import { RocketTeamMember } from './repositories/Rocket-member-repository';
+import { PrismarocketMemberRepository } from './repositories/prisma/Prisma-Rocket-Member-Repository';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [PrismaService]
+  providers: [PrismaService,
+    {
+      provide: RocketTeamMember,
+      useClass: PrismarocketMemberRepository
+    }
+  ]
 })
 export class AppModule { }
